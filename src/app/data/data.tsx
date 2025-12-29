5c4a6d25128536a9f71a7b0fa9c47d62be76a23a
zekScript <armandasdarbinis@gmail.com>
2025-12-29 17:29:10 +0200

    c

--- src/app/data/data.tsx
+++ src/app/data/data.tsx
@@ -7,6 +7,7 @@ export const data = {
                   "React",
                   "Nextjs",
                   "Javascript",
+                  "Typescript",
                   "HTML5",
                   "CSS3",
 
--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -6,12 +6,11 @@ import AboutData from './AboutData'
 
 const About = () => {
   return (
-    <div className='h-125 w-full flex mt-24 gap-12'>
+    <div className='h-125 w-full flex mt-24 gap-12 bg-[#101010] p-4 m-4 rounded shadow-xl'>
             <AboutData/>
             <div>
-                        <h1>
-                          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure quis repudiandae dignissimos incidunt id in voluptas non quisquam reiciendis commodi placeat optio minus excepturi eius, sunt debitis, consequatur et. Harum!
-                        </h1>
+                        <h1>Hi there!</h1>
+                        <p>I like looking at errors and take time solve problems with trial and error. Also love building websites with alot of functionality. My dream life is having fully remote job and camping around with Transit/Sprinter vans. Learning new coding languages and adapting to new tech it's a plus to me, always motivated to work hard.</p>
             </div>
     </div>
   )
